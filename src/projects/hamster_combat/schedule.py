c1eaea7a5ec62cd46de4829b47fcbcc387aa76fb
Lap4Dev <lapchuk.freelancer@gmail.com>
2024-08-10 16:46:11 +0300

    comment

--- src/projects/hamster_combat/schedule.py
+++ src/projects/hamster_combat/schedule.py
@@ -12,7 +12,7 @@ async def hamster_codes_filling_if_necessary(min_codes_count: int):
             generator = PromoCodeGenerator(game_id)
 
             count = await hamster_repo.count_unused_codes(game_id=int(game_id))
-            print(f'Count unused codes for game_id: [{game_id}]: {count}')
+            # print(f'Count unused codes for game_id: [{game_id}]: {count}')
             if min_codes_count > count:
 
                 logger.info(f'Не хватает {min_codes_count-count} кодов для game_id: {game_id}')
