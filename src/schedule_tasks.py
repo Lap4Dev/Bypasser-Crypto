2ef124b1a6083e38212e588cac26916116124171
Lap4Dev <lapchuk.freelancer@gmail.com>
2024-08-10 16:36:08 +0300

    feature: add cron at 0:00 to reset all keys_used stat

--- src/schedule_tasks.py
+++ src/schedule_tasks.py
@@ -6,8 +6,10 @@ from aiogram.types import FSInputFile
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 from src.config import settings, logger
+from src.config.constants import DB_STATISTIC_HAMSTER_GAMES
+from src.loader import db_helper
 from src.projects.hamster_combat.schedule import hamster_codes_filling_if_necessary
-
+from src.repositories import SqlAlchemyStatisticRepository
 hamster_task_lock = asyncio.Lock()
 
 
@@ -33,7 +35,20 @@ async def scheduled_backup_db(bot):
         logger.error(f"An error occurred while sending DB backup: {ex}")
 
 
+async def scheduled_reset_keys_used():
+    try:
+        async with db_helper.get_db() as session:
+            stat_repo = SqlAlchemyStatisticRepository(session)
+
+            for game_id, db_field in DB_STATISTIC_HAMSTER_GAMES.items():
+                await stat_repo.reset_all_by_name(db_field)
+
+    except Exception as ex:
+        logger.error(f'Reset keys used error: {ex}')
+
+
 def setup_schedule_tasks(scheduler: AsyncIOScheduler, bot: Bot):
     scheduler.add_job(scheduled_hamster_task, 'interval', minutes=1)
+    scheduler.add_job(scheduled_reset_keys_used, 'cron', hour=23, minute=0)
     scheduler.add_job(scheduled_backup_db, 'cron', hour=0, minute=0, kwargs={'bot': bot})
     logger.info('Jobs successfully initialized !')
