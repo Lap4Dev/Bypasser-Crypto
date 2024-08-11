3595cad1cc1f3c78740001ca6d07d18c22052122
Lap4Dev <lapchuk.freelancer@gmail.com>
2024-08-11 17:21:21 +0300

    renamed from hamster_combat.py

--- src/handlers/users/__init__.py
+++ src/handlers/users/__init__.py
@@ -5,11 +5,11 @@ def register_routers() -> Router:
     router = Router()
 
     from .commands import router as commands_router
-    from .hamster_combat import router as hamster_combat_router
+    from .hamster_combat_keys import router as hamster_combat__keys_router
     from .referral_program import router as referral_program_router
     router.include_routers(
         commands_router,
-        hamster_combat_router,
+        hamster_combat__keys_router,
         referral_program_router
     )
     return router
