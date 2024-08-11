88f7d422d035257100e7115a5484e673dc458e37
Lap4Dev <lapchuk.freelancer@gmail.com>
2024-08-11 16:41:49 +0300

    add confident_data

--- src/database/models/__init__.py
+++ src/database/models/__init__.py
@@ -4,11 +4,13 @@ from .telegram_user import TelegramUser
 from .partner_channel import PartnerChannel
 from .hamster_code import HamsterCode
 from .statistic import Statistic
+from .confidential_data import ConfidentialData
 
 __all__ = (
     'Base',
     'TelegramUser',
     'PartnerChannel',
     'HamsterCode',
-    'Statistic'
+    'Statistic',
+    'ConfidentialData'
 )
